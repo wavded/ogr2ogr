d08d4f03b1e29f2a74d8a73ee6f3b92b58354041
Marc Harter <wavded@gmail.com>
2013-11-11 14:22:49 -0600

    add; georss, geoconcept, dxf, dgn support

--- test/drivers.js
+++ test/drivers.js
@@ -4,6 +4,7 @@ var fs = require('fs')
 var ogr2ogr = require('../')
 var dir = __dirname+'/samples/'
 var sampleGeoJSONUrl = "https://gist.github.com/wavded/7376428/raw/971548233e441615a426794c766223488492ddb9/test.geojson"
+var sampleGeoRSSUrl = "https://gist.github.com/wavded/7376428/raw/971548233e441615a426794c766223488492ddb9/test.georss"
 
 test('ESRI Shapefile', function (t) {
   t.plan(4)
@@ -115,6 +116,67 @@ test('BNA', function (t) {
   t.plan(3)
 
   ogr2ogr(dir+'sample.bna').format('BNA').exec(function (er, buf) {
+    t.notOk(er, 'no error', { error: er })
+
+    if (buf) {
+      t.ok(Buffer.isBuffer(buf), 'is buffer')
+      t.notEqual(buf[0], 80, 'not in zip format')
+    }
+  })
+})
+
+test('DGN', function (t) {
+  t.plan(2)
+  ogr2ogr(dir+'sample.dgn').exec(function (er, data) {
+    t.notOk(er, 'no error', { error: er })
+    t.ok(data && data.features[0].geometry, 'spatial data')
+  })
+
+  // TODO: DGN Output
+  // ogr2ogr(dir+'sample.dgn').format('DGN').exec(function (er, buf) {
+  //   t.notOk(er, 'no error', { error: er })
+  // })
+})
+
+test('DXF', function (t) {
+  t.plan(2)
+  ogr2ogr(dir+'sample.dgn').exec(function (er, data) {
+    t.notOk(er, 'no error', { error: er })
+    t.ok(data && data.features[0].geometry, 'spatial data')
+  })
+
+  // TODO: DXF Output
+  // ogr2ogr(dir+'sample.dxf').format('DXF').exec(function (er, buf) {
+  //   t.notOk(er, 'no error', { error: er })
+  // })
+})
+
+test('Geoconcept', function (t) {
+  t.plan(2)
+  ogr2ogr(dir+'sample.gxt').exec(function (er, data) {
+    t.notOk(er, 'no error', { error: er })
+    t.ok(data && data.features[0].geometry, 'spatial data')
+  })
+
+  // TODO: Geoconcept Output
+  // ogr2ogr(dir+'sample.dxf').format('Geoconcept').exec(function (er, buf) {
+  //   t.notOk(er, 'no error', { error: er })
+  // })
+})
+
+test('GeoRSS', function (t) {
+  t.plan(7)
+  ogr2ogr(dir+'sample.rss').exec(function (er, data) {
+    t.notOk(er, 'no error', { error: er })
+    t.ok(data && data.features[0].geometry, 'spatial data')
+  })
+
+  ogr2ogr(sampleGeoRSSUrl).exec(function (er, data) {
+    t.notOk(er, 'no error', { error: er })
+    t.ok(data && data.features[0].geometry, 'spatial data')
+  })
+
+  ogr2ogr(dir+'sample.rss').format('GeoRSS').exec(function (er, buf) {
     t.notOk(er, 'no error', { error: er })
 
     if (buf) {
