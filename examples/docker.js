4ddb1a07c4e330b481f95a2197db45c6bda97902
Zach Smith <zachsmith.sa@gmail.com>
2020-11-04 08:46:51 +0200

    Updated sample docker command to use volume mapping

--- examples/docker.js
+++ examples/docker.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const ogr2ogr = require(path.join(__dirname, '../'))
+let tmpdir = require('os').tmpdir()
+if (tmpdir === '/src') tmpdir = '/tmp' // docker issue
 
 ogr2ogr(path.join(__dirname, '../test/samples/sample.shp.zip'))
-  .command('docker run --rm osgeo/gdal:alpine-small-latest ogr2ogr')
+  .command(`docker run -v ${tmpdir}:${tmpdir} --rm osgeo/gdal:alpine-small-latest ogr2ogr`)
   .exec(function (er, data) {
     if (er) {
       console.log('er', er)
