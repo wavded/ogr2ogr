ab61d4b2eaf798cb80d9319f0267e86a88fac12f
rdewit <rdewit@users.noreply.github.com>
2021-05-14 22:42:35 +1000

    Fix custom environment variables logic (#68)
    In the current version ogr2ogr, custom environment variables get ignored due to a bug in the code.

---
+++ test/env-vars-test.js
@@ -0,0 +1,36 @@
+const test = require('tape')
+const ogr2ogr = require('../')
+
+const sampleGeojson = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {area: '51'},
+    },
+  ],
+}
+
+test('env vars can be used', function (t) {
+  t.plan(2)
+
+  ogr2ogr(sampleGeojson)
+    .env({
+      ATTRIBUTES_SKIP: 'NO',
+    })
+    .exec(function (er, data) {
+      t.isEquivalent(
+        data.features[0].properties,
+        sampleGeojson.features[0].properties,
+        'has atrributes'
+      )
+    })
+
+  ogr2ogr(sampleGeojson)
+    .env({
+      ATTRIBUTES_SKIP: 'YES',
+    })
+    .exec(function (er, data) {
+      t.isEquivalent(data.features[0].properties, {}, 'has no atrributes')
+    })
+})
