3c2b86b2957bcc1cec8fa59339c679f3fe8c636c
Marc Harter <wavded@gmail.com>
2021-06-03 09:36:41 -0500

    WIP

--- drivers_test.ts
+++ drivers_test.ts
@@ -1,6 +1,6 @@
 import test from 'blue-tape'
 import ogr2ogr from './ogr2ogr'
-import {Buffer} from 'buffer'
+// import {Buffer} from 'buffer'
 import type {GeoJSON} from 'geojson'
 
 let dir = __dirname + '/testdata/'
@@ -17,7 +17,6 @@ test('GeoJSON', async (t) => {
 
 test('BNA', async (t) => {
   let [data, stderr] = await ogr2ogr(dir + 'sample.bna')
-  t.ok(Buffer.isBuffer(data), 'is buffer')
-  t.notEqual((data as Buffer)[0], 80, 'not in zip format')
+  t.equal((data as GeoJSON).type, 'FeatureCollection')
   t.equal(stderr, '')
 })
